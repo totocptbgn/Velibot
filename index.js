a0e95e3c55ebf7f29a68857c8521c52ebc8ebd97
Thomas Copt-Bignon <35891830+totocptbgn@users.noreply.github.com>
2022-08-10 22:20:52 +0200

    Final fix button

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const https = require('https');
-const { Client, Collection, GatewayIntentBits, InteractionType, EmbedBuilder } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, InteractionType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { token } = require('./config.json');
 
 let stations; // Stations info
@@ -86,6 +86,8 @@ client.on('interactionCreate', async interaction => {
 	// Handling Button (Reaload button)
 	else if (interaction.isButton()) {
 
+		// await interaction.deferUpdate();
+
 		// Get station names
 		const ids = [];
 		const names = [];
@@ -140,8 +142,17 @@ client.on('interactionCreate', async interaction => {
 					.setImage(interaction.message.embeds[0].data.image.url)
 					.setTimestamp()
 					.setFooter({ text: interaction.message.embeds[0].data.footer.text });
-	
-				interaction.update({ embeds: [new_embed], files: [] });
+
+				const row = new ActionRowBuilder()
+					.addComponents(
+						new ButtonBuilder()
+							.setCustomId('reload')
+							.setStyle(ButtonStyle.Primary)
+							.setLabel('　　　　　　Recharger les infos.　　　　　　　'));
+
+				interaction.update({ embeds: [new_embed], files: [], components: []})
+					.then(setTimeout(() => {interaction.editReply({ components: [row]})}, 30000));
+
 			});
 
 		}).on("error", (err) => {
